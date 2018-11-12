7f452bd9a80e69d78c0d67271f814ac1d1ff8af2
Jared Okun <okunj20@gmail.com>
2018-11-12 18:14:03 -0500

    balls are fired from button. added slider for power

--- shoot_ball.js
+++ shoot_ball.js
@@ -3,16 +3,30 @@ var gl;
 var points;
 var vertices=[];
 
+
+var maxBalls = 500;
+
+
 var radius = 0.2;
-var verticesLength;
 
-var maxZ = 400;
 
-var vel = vec3(0, 0.1, 0.2);
+
+var ballIndex = 0;
+
+var maxZ = 50;
+
+var velocities = [];
+
+var initVel = vec3(0, 0.5, 1);
 
 var gravity = vec3(0, -0.01, 0);
 
 var vBuffer;
+
+var powerScale = 0.01;
+
+
+var pointsAroundCircle = 100;
 
 window.onload = function init()
 {
@@ -25,14 +39,17 @@ window.onload = function init()
     //
     //  Configure WebGL
     //
+    document.getElementById("fireButton").onclick = function(){
+    	var power = document.getElementById("power").value;
+		velocities.push(vec3(power * initVel[0], power * initVel[1], power * initVel[2]));
+		vertices.push(vec3(0, 0, 0));
+		for(var i = 0.0; i <= pointsAroundCircle; i += 1.0){
+	        vertices.push(vec3(radius * Math.cos(i * (2.0 * Math.PI)/100.0), radius * Math.sin(i * (2.0 * Math.PI)/100.0), 0));
+	    }
+	    ballIndex++;
+    };
 
-
-    vertices.push(vec3(0, 0, 0));
-
-    for(var i = 0.0; i <= 100; i += 1.0){
-        vertices.push(vec3(radius * Math.cos(i * (2.0 * Math.PI)/100.0), radius * Math.sin(i * (2.0 * Math.PI)/100.0), 0));
-    }
-
+   
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
@@ -46,7 +63,7 @@ window.onload = function init()
     
     vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, vertices.length * 12, gl.STREAM_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, maxBalls * (pointsAroundCircle + 2) * 12, gl.STREAM_DRAW );
 
     
     // Associate out shader variables with our data buffer
@@ -54,7 +71,6 @@ window.onload = function init()
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
-    verticesLength = vertices.length;
 
 
     var maxZLoc = gl.getUniformLocation( program, "maxZ" );
@@ -68,17 +84,35 @@ window.onload = function init()
 
 
 
+
+
 function runPhysics(){
-	for(var i = 0; i < verticesLength; i++){
-    	vertices[i] = add(vertices[i], vel);
+	for(var i = 0; i < velocities.length; i++){
+
+
+
+
+    	for(var j = 0; j < (pointsAroundCircle + 2); j++){
+    		vertices[(pointsAroundCircle + 2) * i + j] = add(vertices[(pointsAroundCircle + 2) * i + j], velocities[i]);
+    	}
+    	velocities[i] = add(velocities[i], gravity);
+
+    	if(vertices[(pointsAroundCircle + 2) * i][2] > maxZ){
+			velocities.splice(i, 1);
+			vertices.splice((pointsAroundCircle + 2) * i, pointsAroundCircle + 2);
+			i--;
+			ballIndex--;
+			
+		}
+
     }
-    vel = add(vel, gravity);
+    
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferSubData( gl.ARRAY_BUFFER, 0, flatten(vertices));
     
     
     render();
-}
+};
 
 
 
@@ -86,8 +120,10 @@ function runPhysics(){
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
 
+    for(var i = 0; i < ballIndex; i++){
+    	gl.drawArrays( gl.TRIANGLE_FAN, i * (pointsAroundCircle + 2), pointsAroundCircle + 2);
+    }
     
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, verticesLength );
     window.requestAnimFrame(runPhysics);
-}
+};
 
