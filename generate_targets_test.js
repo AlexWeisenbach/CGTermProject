de527935e0b63e0e5beaafd3f08383dc22f98015
Alex Weisenbach <weisea3@rpi.edu>
2018-11-26 18:17:05 -0500

    texture with proper alpha channel blending

--- generate_targets_test.js
+++ generate_targets_test.js
@@ -21,6 +21,9 @@ window.onload = function init() {
 
     gl.depthMask(true);
 
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
 
 	var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
